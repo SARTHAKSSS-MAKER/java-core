7c21714345af84b6a5c1688144028e53e0767db4
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-03 02:05:43 +0530

    Learned Java Identifiers

---
+++ 01_Getting_Started/Identifiers.java
@@ -0,0 +1,12 @@
+public class Identifiers{
+    public static void main(String[]
+         args){
+             int age = 20;
+             int studentMarks = 95;
+             double salary = 50000.50;
+             
+             System.out.println(age);
+             System.out.println(studentMarks);
+                System.out.println(salary);
+         }
+}
