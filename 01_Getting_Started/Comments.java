2f727e849f84af6d4680f1bcd0e6bc8743a47f13
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-03 02:29:10 +0530

    Learned Java Comments and Name Conventions

---
+++ 01_Getting_Started/Comments.java
@@ -0,0 +1,13 @@
+public class Comments{
+    public static void main(String []
+args) {
+             
+           //\u000d String name = "Sarthak";
+
+            int age = 20;
+
+            System.out.println(name);
+            System.out.println(age);
+        }
+    
+}
