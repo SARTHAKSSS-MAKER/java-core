3f67e9d17380b5876fe4aa416ac2877fbd7d4d38
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-03 11:40:22 +0530

    Learned variables

---
+++ 02_Java_Basics/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[]
+ args){
+    String name = "Sarthak";
+    int age = 20;
+    double cgpa = 9.5;
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(cgpa);
+ }}
