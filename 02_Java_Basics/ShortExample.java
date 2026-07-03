879604e061b9509113ee63417b11102006fef6d4
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-04 02:31:05 +0530

    Learned Data type - short

---
+++ 02_Java_Basics/ShortExample.java
@@ -0,0 +1,16 @@
+public class ShortExample {
+    
+    public static void main(String[]
+        args) {
+
+            short age = 20;
+            short year = 2026;
+            short people = 10000;
+            short marks = 450;
+
+            System.out.println(age);
+            System.out.println(year);
+            System.out.println(people);
+            System.out.println(marks);
+        }
+}
