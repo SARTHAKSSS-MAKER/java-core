d2e6599f5cbf12922d2b77a0f729222a6d1e110a
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-04 02:35:55 +0530

    Learned Data type - int

---
+++ 02_Java_Basics/IntExample.java
@@ -0,0 +1,11 @@
+public class IntExample {
+    public static void main(String[]
+        args) {
+
+            int age = 20;
+            int salary = 50000;
+
+            System.out.println(age);
+            System.out.print(salary);
+        }
+}
