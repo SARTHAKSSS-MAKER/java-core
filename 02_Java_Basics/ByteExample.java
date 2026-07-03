c3a49d3fc64ca7c1e1a2b3d5eb02f158651956bd
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-04 02:17:00 +0530

    Learned Data type-Bytes

---
+++ 02_Java_Basics/ByteExample.java
@@ -0,0 +1,14 @@
+public class ByteExample {
+
+    public static void main(String[]
+args) {
+
+            byte age = 20;
+            byte marks = 90;
+            byte people = 20;
+            
+            System.out.println(age);
+            System.out.println(marks);
+            System.out.println(people);
+        }
+}
