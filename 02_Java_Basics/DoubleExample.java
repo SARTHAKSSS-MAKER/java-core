35176325f8454ef82b931312af2f0efe51ddfb84
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-05 21:14:21 +0530

    Learned data type - double

---
+++ 02_Java_Basics/DoubleExample.java
@@ -0,0 +1,10 @@
+public class DoubleExample {
+    public static void main(String[]
+        args) {
+            double cgpa = 9.8;
+            double price = 65.99;
+
+            System.out.println(cgpa);
+            System.out.println(price);
+        }
+}
