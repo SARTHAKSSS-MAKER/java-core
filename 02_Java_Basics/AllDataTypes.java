1359aa5da81bfc03024e9adb46757e4aa4cdd4b8
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-05 21:30:36 +0530

    Code of Full Primitive Data type

---
+++ 02_Java_Basics/AllDataTypes.java
@@ -0,0 +1,23 @@
+public class AllDataTypes {
+    public static void main(String[] args) {
+
+        byte age = 25;
+        short year = 2026;
+        int people = 100000;
+        long population = 1450000000L;
+        float percentage = 99.9f;
+        double cgpa = 9.8;
+        char symbol = '$';
+        boolean isPassed = true;
+
+        System.out.println(age);
+        System.out.println(year);
+        System.out.println(people);
+        System.out.println(population);
+        System.out.println(cgpa);
+        System.out.println(percentage);
+        System.out.println(isPassed);
+        System.out.println(symbol);
+    }
+
+}
