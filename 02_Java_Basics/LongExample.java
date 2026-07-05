4ee6db4080fbcb7f88da3884b2bde38260f38a2a
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-05 12:11:29 +0530

    Learned Java data types - long

---
+++ 02_Java_Basics/LongExample.java
@@ -0,0 +1,11 @@
+public class LongExample {
+    public static void main(String[]
+        args) {
+
+            long  population = 1450000000L;
+            long mobileNumber = 8766074303L;
+
+            System.out.println(population);
+            System.out.println(mobileNumber);
+        }
+}
