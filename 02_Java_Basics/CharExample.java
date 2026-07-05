9b4b62a15e3280e9fd7b55709480537912fc1940
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-05 21:18:52 +0530

    Learned dta type - char

---
+++ 02_Java_Basics/CharExample.java
@@ -0,0 +1,11 @@
+public class CharExample {
+    public static void main(String[]
+        args) {
+             char grade = 'A';
+             char symbol = '$';
+
+             System.out.println(grade);
+             System.out.println(symbol);
+        }
+    
+}
