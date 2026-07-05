a66c6958fbbb32d02b4b4f9ba21cb606e47b381e
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-05 21:22:32 +0530

    earned data type-booelan

---
+++ 02_Java_Basics/BooleanExample.java
@@ -0,0 +1,10 @@
+public class BooleanExample {
+    public static void main(String[] args){
+
+        boolean isPassed = true;
+        boolean isLoggedIn = false;
+
+        System.out.println(isPassed);
+        System.out.println(isLoggedIn);
+    }
+}
