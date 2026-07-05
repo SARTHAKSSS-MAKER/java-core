a04e81b6369b5af4dd8cef486099d31e622aeb67
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-06 01:35:51 +0530

    Learned Literals

---
+++ 02_Java_Basics/Literals.java
@@ -0,0 +1,15 @@
+public class Literals {
+    public static void main(String[] args) {
+        int age = 25; 
+        double pi = 3.14;
+        char grade = 'A';
+        boolean isPassed = true;
+        String name = "John";
+
+        System.out.println("Age: " + age);
+        System.out.println("Pi: " + pi);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Passed: " + isPassed);
+        System.out.println("Name: " + name);
+    }
+}
