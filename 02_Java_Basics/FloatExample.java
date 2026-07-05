23b9e9e5b11a56d0b63da0ab049a0925f81d47f1
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-05 21:10:37 +0530

    Learned Data type-float

---
+++ 02_Java_Basics/FloatExample.java
@@ -0,0 +1,11 @@
+public class FloatExample {
+    public static void main(String[]
+        args ) {
+
+            float percentage  = 99.9f;
+            float height = 5.8F;
+
+            System.out.println(percentage);
+            System.out.println(height);
+        }
+}
