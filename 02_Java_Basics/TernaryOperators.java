6c1173ac2890de7599b469d3f8d2732fc711e147
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-08 01:55:01 +0530

    Learned Ternary Operator

---
+++ 02_Java_Basics/TernaryOperators.java
@@ -0,0 +1,10 @@
+public class TernaryOperators {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        String result = (age <= 18) ? "adult" : "minor" ;
+    
+    System.out.println(result);
+    }
+}
