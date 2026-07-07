fd10582b6fbc446dcb921b35c6b82c0ac09b2a48
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-07 16:12:17 +0530

    Learned Logical operators

---
+++ 02_Java_Basics/LogicalOperators.java
@@ -0,0 +1,10 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+
+        System.out.println(10 > 5 && 20 > 10);
+        System.out.println(10 > 5 && 20 < 10);
+        System.out.println(10 < 5 || 20 > 10);
+        System.out.println(!(10 > 5));
+
+    }
+}
