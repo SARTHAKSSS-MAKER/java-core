33ec322616d0caab906ed3546437f2c9c906cf1f
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-09 01:18:40 +0530

    learned explicit conversion

---
+++ 02_Java_Basics/ExplicitConversion.java
@@ -0,0 +1,11 @@
+public class ExplicitConversion {
+    
+    public static void main(String[] args) {
+
+        double cgpa = 8.75;
+        int marks = (int) cgpa;
+
+        System.out.println(cgpa);
+        System.out.println(marks);
+    }
+}
