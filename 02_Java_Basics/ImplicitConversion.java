616fbf70ef84116dc9e99c88743b688b1fec3fdc
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-09 01:15:35 +0530

    Learned implicit conversion

---
+++ 02_Java_Basics/ImplicitConversion.java
@@ -0,0 +1,11 @@
+public class ImplicitConversion {
+    
+    public static void main(String[] args) {
+
+        int a = 10;
+        double b = a;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
