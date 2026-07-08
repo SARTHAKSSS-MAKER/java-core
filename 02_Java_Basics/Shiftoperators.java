f44e05b5f052ec1f1a409622b42d4e9dd0314c66
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-08 15:33:34 +0530

    Learned Shift Operator

---
+++ 02_Java_Basics/Shiftoperators.java
@@ -0,0 +1,13 @@
+public class Shiftoperators {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 20;
+        
+        System.out.println(a << 1);
+        System.out.println(a << 2);
+
+        System.out.println(b >> 1);
+        System.out.println(b >> 2);
+    }
+}
