eaf50ca4b7e7400094330329794a4b23688dc4d9
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-09 01:47:21 +0530

    Learned type promotion

---
+++ 02_Java_Basics/TypePromotion.java
@@ -0,0 +1,15 @@
+public class TypePromotion {
+    
+    public static void main(String[]  args) {
+
+        byte a = 20;
+        byte b = 30;
+        int c = a + b;
+        System.out.println(c);
+
+        int x = 10;
+        double y = 5.5;
+        double z = x + y;
+        System.out.println(z);
+    }
+}
