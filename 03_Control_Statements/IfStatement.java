69c2e74ce8c4cf46f97a9ceb21300f810e26daef
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-10 01:41:08 +0530

    Learned if control statement

---
+++ 03_Control_Statements/IfStatement.java
@@ -0,0 +1,12 @@
+public class IfStatement {
+
+    public static void main(String[] args){
+         
+        int age = 25;
+
+        if(age >= 18){
+            System.out.println("You are eligible to vote.");
+            
+        }
+    }
+}
