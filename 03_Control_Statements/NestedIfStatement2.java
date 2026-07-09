412a56573de9a028575a6a76be248733736cf588
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-10 02:54:07 +0530

    Example 2 nested if control statement

---
+++ 03_Control_Statements/NestedIfStatement2.java
@@ -0,0 +1,18 @@
+public class NestedIfStatement2 {
+    
+    public static void main(String[] args){
+
+        boolean passedExam = true;
+        boolean hasGoodGrade = true;
+        int percentage = 85;
+
+        if(passedExam){
+            if(hasGoodGrade){
+                if(percentage >= 80){
+                    System.out.println("Admission Granted");
+                }
+            }
+        }
+        System.out.println("Thanks Big Dawgggggggggggggggggggggggggggggg");
+    }
+}
