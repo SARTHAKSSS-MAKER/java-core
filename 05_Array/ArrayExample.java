93c9399c688afd74ca608255c09606dc817d3f74
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-14 01:16:10 +0530

    For loop with array

--- 05_Array/ArrayExample.java
+++ 05_Array/ArrayExample.java
@@ -1,6 +1,17 @@
 public class ArrayExample {
     public static void main(String[] args) {
         int[] marks = new int[5];
+        
+        marks[0] = 85;
+        marks[1] = 90;
+        marks[2] = 78;
+        marks[3] = 92;
+        marks[4] = 88;
+
         System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+        System.out.println(marks[4]);
     }
 }
---
+++ 05_Array/ForLoopArray.java
@@ -0,0 +1,15 @@
+public class ForLoopArray {
+    public static void main(String[] args) {
+
+        int[] marks = new int[5];
+        marks[0] = 85;
+        marks[1] = 90;
+        marks[2] = 78;
+        marks[3] = 92;
+        marks[4] = 88;
+
+        for(int i = 0; i < 5; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
