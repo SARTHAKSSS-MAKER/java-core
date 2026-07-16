7f06a510acdbad368636c2675c7c523a70c844b6
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-07-17 02:12:47 +0530

    Array input

---
+++ 05_Array/ArrayInput.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class ArrayInput {
+    public static void main(String[] args) {
+        Scanner input = new
+        Scanner(System.in);
+
+        int[] numbers = new int[5];
+        System.out.println("Enter 5 numbers:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = input.nextInt();
+        }
+            System.out.println("You entered:");
+        
+
+            for (int i = 0; i <numbers.length; i++) {
+                System.out.println(numbers[i]);
+            }
+            input.close();
+        }
+  
+}
