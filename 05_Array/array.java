7ec4b2707a1d5b92d68538faa9af8a89acb8897b
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-08-03 20:01:38 +0530

    -

--- 05_Array/array.java
+++ 05_Array/array.java
@@ -2,4 +2,5 @@ public class array {
     public static void main(String[] args){
          int[] marks = {80,90,78,92,88};
         System.out.println(marks[2]);
+        System.out.println(marks[3]);
     }}
