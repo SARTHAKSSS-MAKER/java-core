820387b50009900082165dd435efdfe9b3d10659
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-08-08 02:09:20 +0530

    maximum subarray sum

---
+++ 05_Array/MaximumSubarraySum.java
@@ -0,0 +1,18 @@
+public class MaximumSubarraySum {
+    public static void main(String[] args) {
+        int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        int maxSum = Integer.MIN_VALUE;
+
+        for ( int i = 0; i < arr.length; i++ ) {
+            int sum = 0;
+            for ( int j = i; j < arr.length; j++ ) {
+                sum = sum + arr[j];
+                if ( sum > maxSum ) {
+                    maxSum = sum;
+                }
+
+            }
+        }
+        System.out.println(maxSum);
+    }
+}
