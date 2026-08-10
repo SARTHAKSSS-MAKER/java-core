8806712bcc10520a9d629f2dd1ded3a6e4ecc31d
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-08-11 01:19:59 +0530

    next permutation

---
+++ 05_Array/NextPermutation.java
@@ -0,0 +1,19 @@
+public class NextPermutation {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        int i = arr.length - 2;
+
+        if ( arr[i] < arr[i + 1] ) {
+            int j = arr.length - 1;
+            while ( arr[j] <= arr[i] ) {
+                j--;
+            }
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            for ( int k = 0; k < arr.length; k++ ) {
+                System.out.print(arr[k] + " ");
+            }
+        }
+        }
+}
