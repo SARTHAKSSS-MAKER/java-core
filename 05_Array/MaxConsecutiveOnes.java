eb3230d1deb9342f85e9891ced1793ad88230095
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-08-25 21:15:04 +0530

    -

--- 05_Array/MaxConsecutiveOnes.java
+++ 05_Array/MaxConsecutiveOnes.java
@@ -2,7 +2,7 @@ public class MaxConsecutiveOnes {
     public static void main(String[] args) {
         int count = 0;
         int max = 0;
-        
+        int[] arr = {1,1,0,1,1,1};
 
         for (int i = 0; i < arr.length; i++) {
             if(arr[i] == 1) {
