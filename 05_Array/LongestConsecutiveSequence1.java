9bb4b2530d4cb579d75776162843761434044986
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-09-16 20:16:27 +0530

    update

---
+++ 05_Array/LongestConsecutiveSequence1.java
@@ -0,0 +1,37 @@
+public class LongestConsecutiveSequence1 {
+    public static void main(String[] args) {
+
+        int[] arr = {100, 4, 200, 1, 3, 2};
+
+        int longest = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            int current = arr[i];
+            int count = 1;
+
+            while (contains(arr, current + 1)) {
+                current++;
+                count++;
+            }
+
+            if (count > longest) {
+                longest = count;
+            }
+        }
+
+        System.out.println(longest);
+    }
+
+    static boolean contains(int[] arr, int target) {
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == target) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
